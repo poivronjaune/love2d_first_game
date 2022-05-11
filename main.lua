5a24d17c831825d4baaa08f67c935a498e86cfdf
poivronjaune <poivronjaune@gmail.com>
2022-05-11 19:50:47 -0400

    Pong-0 scafolding

--- main.lua
+++ main.lua
@@ -1,20 +1,35 @@
+push = require 'push'
+
 -- Physical window on device
 WINDOW_WIDTH   = 1280
 WINDOW_HEIGHT  = 720
 
 -- Adjusted ow res window for retro game
 VIRTUAL_WIDTH  = 384
+VIRTUAL_HEIGHT = VIRTUAL_WIDTH * 0.5625     -- VIRTUAL_HEIGHT = 216 (almost 2:1)
 
 
 function love.load()
-
+    push:setupScreen(
+        VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT,{
+            fullscreen = false,
+            resizable = false,
+            vsync = true
+        })
 end
 
 function love.update(dt)
 
 end
 
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end
+end
 
 function love.draw()
-
+    push:apply('start')
+    love.graphics.printf('Hello pong!', 0, VIRTUAL_HEIGHT / 2 - 6, VIRTUAL_WIDTH, 'center')
+    push:apply('end')
 end
