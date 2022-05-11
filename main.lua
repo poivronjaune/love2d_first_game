8743b1b7a6d19a28734f897b28e678fec49224de
poivronjaune <poivronjaune@gmail.com>
2022-05-10 23:58:51 -0400

    Player movement

--- main.lua
+++ main.lua
@@ -1,26 +1,92 @@
 VIRTUAL_WIDTH  = 384
-VIRTUAL_HEIGHT = 216
+VIRTUAL_HEIGHT = VIRTUAL_WIDTH * 0.5625     -- VIRTUAL_HEIGHT = 216 (almost 2:1)
 WINDOW_WIDTH   = 1280
 WINDOW_HEIGHT  = 720
 
+PADDLE_WIDTH  = 8
+PADDLE_HEIGHT = 32
+PADDLE_SPEED  = 140                         -- Pixels / second
+
+BALL_SIZE     = 4
+
+LARGE_FONT = love.graphics.newFont(25)
+SMALL_FONT = love.graphics.newFont(10)
+
 push = require 'push'
 
+gameState = 'title'
+
+player1 = {
+    x = 10, 
+    y = 10
+}
+player2 = {
+    x = VIRTUAL_WIDTH - PADDLE_WIDTH - player1.x, 
+    y = VIRTUAL_HEIGHT - PADDLE_HEIGHT - player1.y
+}
+ball = {
+    x = VIRTUAL_WIDTH / 2 - BALL_SIZE / 2,
+    y = VIRTUAL_HEIGHT / 2 - BALL_SIZE / 2,
+    dx = 0,
+    dy = 0
+}
+
 function love.load()
+    math.randomseed(os.time())
+    love.graphics.setDefaultFilter('nearest', 'nearest')
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT)
+
+    ball.dx = math.random(60)
+    if math.random(2) == 1 then
+        ball.dx = ball.dx * -1
+    end
+    ball.dy = math.random(60)
+    if math.random(2) == 1 then
+        ball.dy = ball.dy * -1
+    end
+
 end
 
 function love.update(dt)
+    if love.keyboard.isDown('w') then
+        player1.y = math.max(player1.y - PADDLE_SPEED * dt, 0)
+    elseif love.keyboard.isDown('s') then
+        player1.y = math.min(player1.y + PADDLE_SPEED * dt, VIRTUAL_HEIGHT - PADDLE_HEIGHT)
+    end
 
+    if love.keyboard.isDown('up') then
+        player2.y = math.max(player2.y - PADDLE_SPEED * dt, 0)
+    elseif love.keyboard.isDown('down') then
+        player2.y = math.min(player2.y + PADDLE_SPEED * dt, VIRTUAL_HEIGHT - PADDLE_HEIGHT)
+    end    
+
+    if gameState == 'play' then
+        ball.x = ball.x + ball.dx * dt
+        ball.y = ball.y + ball.dx * dt
+    end
 end
 
 function love.keypressed(key)
     if key == "escape" then
         love.event.quit()
-    end
+    end 
+
 end
 
 function love.draw()
     push:start()
-    love.graphics.print('Hello World!')
+    -- love.graphics.print('Hello World!')
+    love.graphics.clear(40/255, 45/255, 52/255, 255/255)
+
+    if gameState == 'title' then
+        love.graphics.setFont(LARGE_FONT)
+        love.graphics.printf('Love2D Pong', 0, 10, VIRTUAL_WIDTH, 'center')
+        love.graphics.setFont(SMALL_FONT)
+        love.graphics.printf('Press ENTER to start', 0, VIRTUAL_HEIGHT - 32, VIRTUAL_WIDTH, 'center')
+    end
+
+    love.graphics.rectangle('fill', player1.x, player1.y, PADDLE_WIDTH, PADDLE_HEIGHT)
+    love.graphics.rectangle('fill', player2.x, player2.y, PADDLE_WIDTH, PADDLE_HEIGHT)
+    love.graphics.rectangle('fill', ball.x, ball.y, BALL_SIZE, BALL_SIZE)
     push:finish()
 end
