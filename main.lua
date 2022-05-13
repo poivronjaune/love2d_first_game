39aafcebdbe519de21e2eb7934b386d7aa3a8f04
poivronjaune <poivronjaune@gmail.com>
2022-05-12 20:41:42 -0400

    tmp commit

--- main.lua
+++ main.lua
@@ -8,6 +8,23 @@ WINDOW_HEIGHT  = 720
 VIRTUAL_WIDTH  = 384
 VIRTUAL_HEIGHT = VIRTUAL_WIDTH * 0.5625     -- VIRTUAL_HEIGHT = 216 (almost 2:1)
 
+-- Animation constants
+PADDLE_WIDTH  = 8
+PADDLE_HEIGHT = 32
+PADDLE_START_POS_X = 15
+PADDLE_START_POS_Y = 20 
+PADDLE_SPEED  = 200     -- Pixels per second
+
+player1 = {
+    x = PADDLE_START_POS_X,
+    y = PADDLE_START_POS_Y,
+    score = 0
+}
+player2 = {
+    x = VIRTUAL_WIDTH - PADDLE_START_POS_X - PADDLE_WIDTH,
+    y = VIRTUAL_HEIGHT - PADDLE_START_POS_Y - PADDLE_HEIGHT,
+    score = 0
+}
 
 function love.load()
     -- Adjust resolution for anti aliasing
@@ -27,7 +44,7 @@ function love.load()
 end
 
 function love.update(dt)
-
+    
 end
 
 function love.keypressed(key)
@@ -43,8 +60,8 @@ function love.draw()
     love.graphics.clear(40/255, 45/255, 52/255, 255/255)
     -- Display paddles, ball and game title
     love.graphics.printf('Hello pong!', 0, 10, VIRTUAL_WIDTH, 'center')
-    love.graphics.rectangle('fill', 10, 50, 5, 20)
-    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 50, 5, 20)
+    love.graphics.rectangle('fill', player1.x, player1.y, 5, 20)
+    love.graphics.rectangle('fill', player2.x, player2.y, 5, 20)
     love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
     push:apply('end')
 end
